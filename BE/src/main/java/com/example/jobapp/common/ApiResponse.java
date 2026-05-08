6fd1a0cc07481f07d499568a3d0138ef5f7bd9a4
Truong back-end <honguyententhanhtruong@gmail.com>
2026-05-08 13:04:29 +0700

    a

--- BE/src/main/java/com/example/jobapp/common/ApiResponse.java
+++ BE/src/main/java/com/example/jobapp/common/ApiResponse.java
@@ -54,6 +54,6 @@ public class ApiResponse<T> {
 
     public static ApiResponse<Void> validationError(String message, Map<String, String> errors) {
         return new ApiResponse<>(false, message, errors);
-        /// /
+
     }
 }
